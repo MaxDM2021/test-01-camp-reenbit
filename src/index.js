459f8a97b09f3821d4f7f9cacf91ba9a0bdb49ab
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2023-03-11 21:27:53 +0200

    2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Test-Camp-Reenbit">
+    <BrowserRouter basename="/test-01-camp-reenbit">
       <App />
     </BrowserRouter>
   </React.StrictMode>
